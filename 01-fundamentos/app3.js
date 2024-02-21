4becca653c33b071682b69bf8ba683e894c83ae0
steeltx <realsteel.gnu@gmail.com>
2024-02-20 22:48:45 -0600

    Se agrega sección de introducción

---
+++ 01-fundamentos/app3.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const content = fs.readFileSync('./README.md', 'utf-8');
+const words = content.split(' ');
+
+// const reactWordCount = words.filter(
+//     word => word.toLocaleLowerCase() == 'react'
+// ).length;
+
+const reactWordCount = content.match(/react/gi ?? []).length;
+
+console.log('Palabras : '+words.length);
+console.log('Palabras React : '+reactWordCount);
